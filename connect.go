1e7e20121403079b4c3e2026ba50209984b01b30
Udam Liyanage <udam1998@gmail.com>
2020-01-04 22:58:48 +0530

    Database connection logic

---
+++ connect.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+func connect() *mongo.Collection {
+	clientOptions := options.Client().ApplyURI("mongodb+srv://user-service:RCIcEjYEtMJTuhHS@platform-test-5fvzo.gcp.mongodb.net/test?retryWrites=true&w=majority")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection := client.Database("Organisations").Collection("organisations")
+	return collection
+}
