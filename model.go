fbb1f2ae29d595cbf396d6920a23a6d57a258629
Udam Liyanage <udam1998@gmail.com>
2020-01-05 19:25:10 +0530

    Model for device attach

--- model.go
+++ model.go
@@ -14,3 +14,9 @@ type AttachOwner struct {
 	UserName       string             `json:"user_name" bson:"user_name"`
 	OrganisationID primitive.ObjectID `json:"organisation_id" bson:"organisation_id"`
 }
+
+type AttachDevice struct {
+	DeviceID       string             `json:"device_id" bson:"device_id"`
+	DeviceName     string             `json:"device_name" bson:"device_name"`
+	OrganisationID primitive.ObjectID `json:"organisation_id" bson:"organisation_id"`
+}
