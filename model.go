c7c2b8f19eed283df697120b0905d512fcfdadb0
Udam Liyanage <udam1998@gmail.com>
2020-01-05 07:25:54 +0530

    Updates to model to fit empty fields

--- model.go
+++ model.go
@@ -5,6 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Organisation struct {
 	ID      primitive.ObjectID  `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name    string              `json:"name" bson:"name"`
-	Devices []map[string]string `json:"devices,omitempty" bson:"devices,omitempty"`
-	Users   []map[string]string `json:"users,omitempty" bson:"users,omitempty"`
+	Devices []map[string]string `json:"devices" bson:"devices"`
+	Users   []map[string]string `json:"users" bson:"users"`
 }
