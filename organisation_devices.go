e3b6c284fac8e70a5e1cb7140ab4d0a1119bdd29
Udam Liyanage <udam1998@gmail.com>
2020-01-05 19:42:05 +0530

    Get attached devices handler

--- organisation_devices.go
+++ organisation_devices.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func attachDevice(c *gin.Context) {
@@ -30,5 +32,22 @@ func attachDevice(c *gin.Context) {
 }
 
 func getAttachedDevices(c *gin.Context) {
-	c.String(200, "Get Attached Devices")
+	var res []map[string]interface{}
+	objID, err := primitive.ObjectIDFromHex(c.Param("id"))
+	checkError(err, c)
+	cursor, err := db.Collection.Find(
+		context.Background(),
+		bson.M{"_id": objID},
+		options.Find().SetProjection(bson.D{
+			{"devices", 1},
+		}),
+	)
+	checkError(err, c)
+	for cursor.Next(context.TODO()) {
+		var user map[string]interface{}
+		err := cursor.Decode(&user)
+		checkError(err, c)
+		res = append(res, user)
+	}
+	c.JSON(200, res)
 }
