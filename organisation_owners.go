67902532620decbe9e14613060e0c91963cbca5b
Udam Liyanage <udam1998@gmail.com>
2020-01-05 08:18:47 +0530

    Attach user model and working feature

--- organisation_owners.go
+++ organisation_owners.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func attachUser(c *gin.Context) {
-	c.JSON(200, "Attach Organisation Function")
+	var attach AttachOwner
+	err := json.NewDecoder(c.Request.Body).Decode(&attach)
+	if err != nil {
+		panic(err)
+	}
+	filter := bson.M{"_id": attach.OrganisationID}
+	update := bson.M{
+		"$addToSet": bson.M{
+			"users": bson.M{
+				"user_id":   attach.UserID,
+				"user_name": attach.UserName,
+			},
+		},
+	}
+	res, err := db.Collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(201, res)
 }
