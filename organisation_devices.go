ab3777f2e88b27e5a3fe63620b3baf53e01d2e63
Udam Liyanage <udam1998@gmail.com>
2020-01-05 19:37:58 +0530

    Attach Device Function

--- organisation_devices.go
+++ organisation_devices.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func attachDevice(c *gin.Context) {
-	c.String(200, "Attach Device Function")
+	var attach AttachDevice
+	err := json.NewDecoder(c.Request.Body).Decode(&attach)
+	if err != nil {
+		panic(err)
+	}
+	filter := bson.M{"_id": attach.OrganisationID}
+	update := bson.M{
+		"$addToSet": bson.M{
+			"devices": bson.M{
+				"device_id":   attach.DeviceID,
+				"device_name": attach.DeviceName,
+			},
+		},
+	}
+	res, err := db.Collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(201, res)
 }
