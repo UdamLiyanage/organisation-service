c9f8c2388ca61a9aa6e5282c4e9da995b48a19cd
Udam Liyanage <udam1998@gmail.com>
2020-01-10 20:15:12 +0530

    Optimisation - Update operation response formatting

--- update_organisation.go
+++ update_organisation.go
@@ -20,7 +20,7 @@ func updateOrganisation(c *gin.Context) {
 			"name": organisation.Name,
 		},
 	}
-	_, err = DB.Collection.UpdateOne(context.TODO(), filter, update)
+	res, err := DB.Collection.UpdateOne(context.TODO(), filter, update)
 	checkError(err, c)
-	c.Status(200)
+	c.JSON(200, res)
 }
